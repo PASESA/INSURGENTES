88c44a42b41edfb0fe9c85b46b83607bfeacbda9
Noé Zúñiga <noe.zm2000@gmail.com>
2023-09-29 14:42:36 -0600

    Fix: se corrige error al calcular el corte

--- Cobro/cobroFO.py
+++ Cobro/cobroFO.py
@@ -1309,6 +1309,7 @@ class FormularioOperacion:
 
 
     def Guardar_Corte(self):
+        self.Calcular_Corte()
         self.Puertoycontar()
 
         ######Obtenemos los datos del Cajero en Turno
